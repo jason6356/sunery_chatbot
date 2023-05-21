0642a05b19533bdc9258334e0674a71e39723d68
jason6356 <70106185+jason6356@users.noreply.github.com>
2023-05-21 16:30:06 +0800

    Update flaskServer.py

--- flaskServer.py
+++ flaskServer.py
@@ -7,7 +7,11 @@ import csv
 nlp = spacy.load('en_core_web_md')
 
 app = Flask(__name__)
-CORS(app, origins=['*'], methods=['GET','POST])
+CORS(app, origins=['*'], methods=['GET','POST]) 
+                                  
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
 
 # Define the route for the chatbot endpoint
 @app.route('/chatbot', methods=['POST'])
